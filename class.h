bb5ebeea90bb9384df73fd598bfd55cf59cc5dd3
neo1691 <anubhav1691@gmail.com>
2012-05-15 15:33:43 +0530

    Basic Algo

---
+++ class.h
@@ -0,0 +1,49 @@
+#ifndef CLASS_H_INCLUDED
+#define CLASS_H_INCLUDED
+
+#include <iostream>
+#include <cstring>
+#define SIZE 1000
+using namespace std;
+
+class Encrypt
+{
+	char str[SIZE];
+	unsigned len;
+public:
+	void display (void);
+	void accept (void);
+	void encryptStr (void);
+	void decryptStr (void);
+};
+
+void Encrypt::display(void)
+{
+	cout<< "String is: "<<str;
+	cout<<endl;
+}
+
+void Encrypt::accept(void)
+{
+    cout<< "Enter the string: ";
+    cin>>str;
+    len=strlen(str);
+    cout<<endl;
+}
+
+void Encrypt::encryptStr(void)
+{
+    int i;
+    for(i=0;i<len;i++)
+        str[i]=str[i]+30;
+    str[i]='\0';
+}
+
+void Encrypt::decryptStr(void)
+{
+    int i;
+    for(i=0;i<len;i++)
+        str[i]=str[i]-30;
+    str[i]='\0';
+}
+#endif // CLASS_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "class.h"
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    Encrypt test;
+    int ch;
+    do {
+    cout << "1) Accept String" << endl;
+    cout << "2) Encrypt String" << endl;
+    cout << "3) Decrypt String" << endl;
+    cout << "4) Display String" << endl;
+    cout << "5) Exit" << endl;
+    cout<< "Enter yotu choice: ";
+
+    cin>>ch;
+    cout<<endl;
+    switch(ch)
+    {
+        case 1:
+        test.accept();
+        break;
+        case 2:
+        test.encryptStr();
+        break;
+        case 3:
+        test.decryptStr();
+        break;
+        case 4:
+        test.display();
+        break;
+        case 5:
+        break;
+        default:
+        cout<< "Incorrect Param"<<endl;
+    }}while(ch!=5);
+    return 0;
+}
